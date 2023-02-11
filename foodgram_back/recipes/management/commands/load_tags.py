6c65b44818c6f9b1812264bcd4b8011b8abfd735
Александр Буваев <aleksandrbuvaev7@gmail.com>
2023-02-11 12:42:03 +0300

    add load_tags command

---
+++ foodgram_back/recipes/management/commands/load_tags.py
@@ -0,0 +1,17 @@
+from recipes.models import Tag
+
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Загрузка подготовленных тегов.'
+
+    def handle(self, *args, **kwargs):
+        data = [
+            {'name': 'Завтрак', 'color': '#EC6E19', 'slug': 'breakfast'},
+            {'name': 'Обед', 'color': '#DB04E9', 'slug': 'dinner'},
+            {'name': 'Ужин', 'color': '#364236', 'slug': 'evening_meal'}
+        ]
+        Tag.objects.bulk_create(
+            Tag(**value) for value in data)
+        self.stdout.write(self.style.SUCCESS('Все теги загружены.'))
